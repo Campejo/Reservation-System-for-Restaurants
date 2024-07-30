0169204b07c66a1b397f057d2fe9f1a5a335cb91
Thiago Campello <campellothiago@hotmail.com>
2024-07-29 21:38:03 -0300

    Change the run class name

--- src/main/java/com/myproject/Application.java
+++ src/main/java/com/myproject/Application.java
@@ -1,15 +1,10 @@
 package com.myproject;
 
 import com.myproject.model.Login;
-import com.myproject.model.Restaurant;
-import com.myproject.model.RestaurantReservation;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.time.LocalDateTime;
-
 @SpringBootApplication
-public class ReservationSystemForRestaurantsApplication {
+public class Application {
 
 	public static void main(String[] args) {
 		//SpringApplication.run(ReservationSystemForRestaurantsApplication.class, args);
